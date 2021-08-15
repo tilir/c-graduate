baa34558b28abadca210f07a8bfdafe12b76cbbd
Tilir <konstantin.vladimirov@gmail.com>
2021-08-15 19:22:42 +0300

    Fixing euclid programm

--- 01-basics/euclid.c
+++ 01-basics/euclid.c
@@ -1,14 +1,24 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int gcd(int x, int y) {
-  assert((x > y) && (y > 0));
-  int q = x % y;
+  int q;
+  assert(y != 0);
+  q = x % y;
   if (q == 0)
     return y;
   return gcd(y, q);
 }
 
 int main() {
-  printf("%d\n", gcd(1769, 427));
+  int x, y, g, res;
+  res = scanf("%d%d", &x, &y);
+  if (res != 2 || y == 0) {
+    printf("%s\n", "Wrong input");
+    abort();
+  }
+  g = gcd(x, y);
+  printf("gcd(%d, %d) = %d\n", x, y, g);
+  return 0;
 }
