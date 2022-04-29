3b9a958926f86fbcec729ad37309e00b96725203
Tilir <konstantin.vladimirov@gmail.com>
2022-04-29 20:06:33 +0300

    Adding intra-register sorting to SIMD examples

--- simd/basic_tests.c
+++ simd/basic_tests.c
@@ -28,7 +28,7 @@ void set_and_print() {
   y = set_yvalue(8);
   DUMP_YREG(y);
   z = set_zvalue(16);
-  DUMP_ZREG(z);  
+  DUMP_ZREG(z);
 }
 
 void store_and_load() {
@@ -39,25 +39,27 @@ void store_and_load() {
   printf("%s\n", "store and load test");
   z0 = set_zvalue(16);
   z1 = set_zvalue(20);
-  store_zvalue(vals, z0);  
+  store_zvalue(vals, z0);
   z2 = mask_load_zvalue(z1, vals, mask); // load and blend with z1
   DUMP_ZREG(z2);
-  z3 = blend_zvalue(mask, z1, z0);
+  z3 = blend_zvalues(mask, z1, z0);
   assert(equal_zvalue(z2, z3));
-  z3 = blend_zvalue(mask, z0, z1);
+  z3 = blend_zvalues(mask, z0, z1);
   DUMP_ZREG(z3);
 }
 
 void permute() {
   ri512 z0, z1, z2, z3;
   printf("%s\n", "permute test");
-  z0 = set_zvalues(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
-  z1 = set_zvalues(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-  z2 = permute_zvalues(z0, z1); // equal to z1, as z0 is identity
-  z3 = permute_zvalues(z1, z0); // equal to z1, as z1 is reverse 
-  DUMP_ZREG(z2);
-  assert(equal_zvalue(z1, z2));
+  z0 = setr_zvalues(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+  z1 = setr_zvalues(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+  z2 = permute_zvalues(z1, z0); // equal to z0, as z1 is identity
+  z3 = permute_zvalues(z0, z1); // equal to z0, as z0 is reverse
+  assert(equal_zvalue(z0, z2));
   assert(equal_zvalue(z2, z3));
+  z2 = setr_zvalues(3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12);
+  z3 = set_zvalues(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+  DUMP_ZREG(permute_zvalues(z2, z3));
 }
 
 void rotate_shift() {
@@ -75,10 +77,36 @@ void rotate_shift() {
   DUMP_ZREG(z2);
 }
 
+void arithmetics() {
+  ri512 z0, z1, z2, z3;
+  printf("%s\n", "arithmetics test");
+  z0 = set_zvalue(10);
+  z1 = add_zvalues(z0, z0);
+  assert(equal_zvalue(z1, set_zvalue(20)));
+  z1 = set_zvalues(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+  z2 = min_zvalues(z0, z1);
+  DUMP_ZREG(z2);
+  z3 = max_zvalues(z0, z1);
+  DUMP_ZREG(z3);
+}
+
+void sorts() {
+  ri512 z0, z1, z2;
+  printf("%s\n", "sort test");
+  z0 = set_zvalues(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+  z1 = set_zvalues(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+  z2 = set_zvalues(0, 2, 14, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 15);
+  DUMP_ZREG(sort_two_lanes_of_8(z0));
+  DUMP_ZREG(sort_two_lanes_of_8(z1));
+  DUMP_ZREG(sort_two_lanes_of_8(z2)); // 1 8 9 ... 15 0 2 3 ... 14
+}
+
 int main() {
   set_and_print();
   store_and_load();
   permute();
   rotate_shift();
+  arithmetics();
+  sorts();
   printf("%s\n", "done");
 }
--- simd/simd.h
+++ simd/simd.h
@@ -9,6 +9,7 @@
 // SIMD header (assumed Intel intrinsics)
 //
 // see also: intel.com/content/www/us/en/docs/intrinsics-guide/index.html
+// see also: github.com/BobSteagall/experimental/blob/master/simd
 //
 //-----------------------------------------------------------------------------
 
@@ -40,8 +41,8 @@ _Static_assert(CHAR_BIT == 8, "Only 8-bit bytes allowed");
 _Static_assert(sizeof(int) == 4, "Only 4-byte ints allowed");
 
 // special constants
-// note: 0xFF excessive for m8 if we are using ints, 
-//       since there are only 4 ints in m128 so 0xF will do. 
+// note: 0xFF excessive for m8 if we are using ints,
+//       since there are only 4 ints in m128 so 0xF will do.
 //       But we want to account for general case.
 enum {
   M8_ALLONES = 0xFFu,
@@ -49,6 +50,12 @@ enum {
   M32_ALLONES = 0xFFFFFFFFu,
   M64_ALLONES = 0xFFFFFFFFFFFFFFFFu,
 };
+
+// make simple bitmasks
+#define make_bitmask(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P)           \
+  ((A << 0) | (B << 1) | (C << 2) | (D << 3) | (E << 4) | (F << 5) |           \
+   (G << 6) | (H << 7) | (I << 8) | (J << 9) | (K << 10) | (L << 11) |         \
+   (M << 12) | (N << 13) | (O << 14) | (P << 15))
 
 // dumps to stdout
 #define DUMP_XREG(R) print_xreg(stdout, #R, R)
@@ -70,6 +77,11 @@ static inline ri512 set_zvalue(int i) { return _mm512_set1_epi32(i); }
 #define set_yvalues(...) _mm256_set_epi32(__VA_ARGS__)
 #define set_zvalues(...) _mm512_set_epi32(__VA_ARGS__)
 
+// sets all values reverse
+#define setr_xvalues(...) _mm_setr_epi32(__VA_ARGS__)
+#define setr_yvalues(...) _mm256_setr_epi32(__VA_ARGS__)
+#define setr_zvalues(...) _mm512_setr_epi32(__VA_ARGS__)
+
 // stores and loads (see alignment requirements in definition)
 static inline void mask_store_xvalue(void *addr, m8 mask, ri128 val);
 static inline void store_xvalue(void *addr, ri128 val);
@@ -86,13 +98,15 @@ static inline ri512 mask_load_zvalue(ri512 src, void *addr, m16 mask);
 static inline ri512 load_zvalue(void *addr);
 
 // blends and permutations
-static inline ri128 blend_xvalue(m8 mask, ri128 src1, ri128 src2);
-static inline ri256 blend_yvalue(m8 mask, ri256 src1, ri256 src2);
-static inline ri512 blend_zvalue(m16 mask, ri512 src1, ri512 src2);
+#define blend_xvalues(mask, src1, src2) _mm_mask_blend_epi32(mask, src1, src2)
+#define blend_yvalues(mask, src1, src2)                                        \
+  _mm256_mask_blend_epi32(mask, src1, src2)
+#define blend_zvalues(mask, src1, src2)                                        \
+  _mm512_mask_blend_epi32(mask, src1, src2)
 
-static inline ri128 permute_xvalues(ri128 idx, ri128 src);
-static inline ri256 permute_yvalues(ri256 idx, ri256 src);
-static inline ri512 permute_zvalues(ri512 idx, ri512 src);
+#define permute_xvalues(idx, src) _mm_permutexvar_epi32(idx, src)
+#define permute_yvalues(idx, src) _mm256_permutexvar_epi32(idx, src)
+#define permute_zvalues(idx, src) _mm512_permutexvar_epi32(idx, src)
 
 // aggregate comparisons: results are 1 or 0
 static inline int less_xvalue(ri128 src1, ri128 src2);
@@ -109,6 +123,12 @@ static inline ri512 shift_zvalue(ri512 r0, int amt);
 // arithmetics (mostly 1-to-1)
 #define add_zvalues(x, y) _mm512_add_epi32(x, y)
 #define sub_zvalues(x, y) _mm512_sub_epi32(x, y)
+#define max_zvalues(x, y) _mm512_max_epi32(x, y)
+#define min_zvalues(x, y) _mm512_min_epi32(x, y)
+
+// sorting
+static inline ri512 sort_pairwise(ri512 vals, ri512 perm, m32 mask);
+static inline ri512 sort_two_lanes_of_8(ri512 vals);
 
 //-----------------------------------------------------------------------------
 //
@@ -222,23 +242,6 @@ ri512 mask_load_zvalue(ri512 src, void *addr, m16 mask) {
 // addr shall be 64-byte aligned
 ri512 load_zvalue(void *addr) { return _mm512_load_epi32(addr); }
 
-// blends
-ri128 blend_xvalue(m8 mask, ri128 src1, ri128 src2) {
-  return _mm_mask_blend_epi32(mask, src1, src2);
-}
-
-ri256 blend_yvalue(m8 mask, ri256 src1, ri256 src2) {
-  return _mm256_mask_blend_epi32(mask, src1, src2);
-}
-
-ri512 blend_zvalue(m16 mask, ri512 src1, ri512 src2) {
-  return _mm512_mask_blend_epi32(mask, src1, src2);
-}
-
-ri512 permute_zvalues(ri512 idx, ri512 src) {
-  return _mm512_permutexvar_epi32(idx, src);
-} 
-
 // comparisons
 int less_zvalue(ri512 src1, ri512 src2) {
   m16 mask = _mm512_cmp_epi32_mask(src1, src2, _MM_CMPINT_NLE);
@@ -256,7 +259,7 @@ ri512 rotate_zvalue(ri512 r0, int amt) {
   int shift, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P;
   if ((amt % 16) == 0)
     return r0;
-  shift = (amt > 0) ? (16 - (amt % 16)) : -amt;  
+  shift = (amt > 0) ? (16 - (amt % 16)) : -amt;
   A = (shift + 0) % 16;
   B = (shift + 1) % 16;
   C = (shift + 2) % 16;
@@ -285,9 +288,57 @@ ri512 shift_zvalue(ri512 r0, int amt) {
 
   if (amt < 0) {
     shiftmask = (M16_ALLONES << (unsigned)(-amt)) & M16_ALLONES;
-    return blend_zvalue(shiftmask, set_zvalue(0), rotate_zvalue(r0, amt));
+    return blend_zvalues(shiftmask, set_zvalue(0), rotate_zvalue(r0, amt));
   }
 
   shiftmask = (M16_ALLONES << (unsigned)(16 - amt)) & M16_ALLONES;
-  return blend_zvalue(shiftmask, rotate_zvalue(r0, amt), set_zvalue(0));
+  return blend_zvalues(shiftmask, rotate_zvalue(r0, amt), set_zvalue(0));
+}
+
+// sorting
+
+// pairwise sort: mask is smth like: 0 1 0 1 0 1 ...
+//                permutation is like 14, 15, 12, 13 ....
+//                outputs min(v0, v1) max(v0, v1) ....
+ri512 sort_pairwise(ri512 vals, ri512 perm, m32 mask) {
+  ri512 exch = permute_zvalues(perm, vals);
+  ri512 vmin = min_zvalues(vals, exch);
+  ri512 vmax = max_zvalues(vals, exch);
+  return blend_zvalues(mask, vmin, vmax);
+}
+
+// sort two lanes
+ri512 sort_two_lanes_of_8(ri512 vals) {
+  ri512 perm0, perm1, perm2, perm3, perm4, perm5;
+  m32 mask0, mask1, mask2, mask3, mask4, mask5;
+
+  perm0 = setr_zvalues(1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14);
+  mask0 = make_bitmask(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
+
+  perm1 = setr_zvalues(3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12);
+  mask1 = make_bitmask(0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1);
+
+  perm2 = setr_zvalues(1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14);
+  mask2 = make_bitmask(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
+
+  perm3 = setr_zvalues(7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8);
+  mask3 = make_bitmask(0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1);
+
+  perm4 = setr_zvalues(2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13);
+  mask4 = make_bitmask(0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1);
+
+  perm5 = setr_zvalues(1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14);
+  mask5 = make_bitmask(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
+
+  DUMP_ZREG(vals);
+  vals = sort_pairwise(vals, perm0, mask0);
+  DUMP_ZREG(vals);
+  vals = sort_pairwise(vals, perm1, mask1);
+  DUMP_ZREG(vals);
+  vals = sort_pairwise(vals, perm2, mask2);
+  vals = sort_pairwise(vals, perm3, mask3);
+  vals = sort_pairwise(vals, perm4, mask4);
+  vals = sort_pairwise(vals, perm5, mask5);
+
+  return vals;
 }
